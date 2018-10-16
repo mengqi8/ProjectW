06ff700b7a47cb9a0468892fefa8caa30fb6a508
xkdzy2002 <107829417@qq.com>
2018-10-17 02:11:29 +0800

    增加Springboot项目工程文件

---
+++ src/main/java/com/unicom/projectw/ProjectwApplication.java
@@ -0,0 +1,12 @@
+package com.unicom.projectw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectwApplication.class, args);
+    }
+}
